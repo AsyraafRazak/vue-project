1ccf4b6edfbc3015fe6737cb647a480b1f113d78
DanishDa22 <danish.dnial800@gmail.com>
2026-08-16 16:24:00 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
-  },
-  server: {
-    watch: {
-      ignored: ['/.vs/'],
+    server: {
+        watch: {
+            ignored: ['**/.vs/**'],
+        },
     },
-  },
 })
