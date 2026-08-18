207c2d728fdca3e06c54204198cc30160c3653e6
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2026-08-19 00:34:18 +0800

    - fix navbar (demo)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -63,8 +63,7 @@
             <div v-if="isMenuOpen" class="mobile-drawer">
                 <ul class="mobile-nav-links">
                     <li><a href="/#features" class="mobile-nav-link" @click="closeMenu">Features</a></li>
-                    <li><a href="/#benefits" class="mobile-nav-link" @click="closeMenu">Benefits</a></li>
-                    <li><a href="/#pricing" class="mobile-nav-link" @click="closeMenu">Pricing</a></li>
+                    <li><router-link to="/demo" class="nav-link">Demo</router-link></li>
                     <li><router-link to="/contact" class="mobile-nav-link" @click="closeMenu">Contact</router-link></li>
                     <li class="mobile-cta-li">
                         <router-link to="/contact" class="btn btn-primary mobile-cta" @click="closeMenu">Get Started</router-link>
