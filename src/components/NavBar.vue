79b4682e26d462ba0e2e1b58b5e8f0e844957d40
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2026-08-19 00:39:39 +0800

    - change navbar (contact us)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -42,12 +42,11 @@
                 <li><a href="/#features" class="nav-link">Features</a></li>
                 <li><router-link to="/demo" class="nav-link">Demo</router-link></li>
                 <li><a href="/#pricing" class="nav-link">Pricing</a></li>
-                <li><router-link to="/contact" class="nav-link">Contact</router-link></li>
             </ul>
 
             <!-- Desktop CTA Button -->
             <div class="nav-cta">
-                <router-link to="/contact" class="btn btn-primary">Get Started</router-link>
+                <router-link to="/contact" class="btn btn-primary">Contact Us</router-link>
             </div>
 
             <!-- Hamburger Menu Button -->
@@ -64,9 +63,8 @@
                 <ul class="mobile-nav-links">
                     <li><a href="/#features" class="mobile-nav-link" @click="closeMenu">Features</a></li>
                     <li><router-link to="/demo" class="mobile-nav-link" @click="closeMenu">Demo</router-link></li>
-                    <li><router-link to="/contact" class="mobile-nav-link" @click="closeMenu">Contact</router-link></li>
                     <li class="mobile-cta-li">
-                        <router-link to="/contact" class="btn btn-primary mobile-cta" @click="closeMenu">Get Started</router-link>
+                        <router-link to="/contact" class="btn btn-primary mobile-cta" @click="closeMenu">Contact Us</router-link>
                     </li>
                 </ul>
             </div>
